8f83e424f50f41d667f5ca2d5bc84564d54332a6
Boris M. Yartsev <borboss@gmail.com>
2019-04-04 18:57:01 +0300

    fix

--- http-port-query.sh
+++ http-port-query.sh
@@ -1,4 +1,4 @@
-curl -H accept:application/json -H content-type:application/json \
-     -d '{"statements":[{"statement":"MATCH (u:User) RETURN u LIMIT 1"}]}' \
+curl -H accept:application/json -H content-type:application/json -u neo4j:tail-analyses-experiences \
+     -d '{"statements":[{"statement":"MATCH (u:User) RETURN u"}]}' \
      http://52.3.253.48:37880/db/data/transaction/commit \
      | jq -r '(.results[0]) | .columns,.data[].row | @csv'
