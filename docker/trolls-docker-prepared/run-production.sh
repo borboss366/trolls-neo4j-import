84bca5e3a1c93f217ef1458b3419970cf4eb1b23
Boris M. Yartsev <borboss@gmail.com>
2019-04-15 15:08:03 +0300

    restart docker script

--- docker/trolls-docker-prepared/run-production.sh
+++ docker/trolls-docker-prepared/run-production.sh
@@ -1,1 +1,2 @@
+sudo docker stop $(sudo docker ps -q --filter ancestor=borisyartsev9taxify/neo4j-stuff:neo4j-clean-1)
 sudo docker run -d -p 7474:7474 -p 7687:7687 borisyartsev9taxify/neo4j-stuff:trolls-1
